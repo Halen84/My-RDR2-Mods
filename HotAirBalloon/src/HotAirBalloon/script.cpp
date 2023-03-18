139a9b701f594963894dbf87a49de701af703331
Halen84 <55899402+Halen84@users.noreply.github.com>
2023-03-18 17:15:02 -0500

    Update script.cpp

--- HotAirBalloon/src/HotAirBalloon/script.cpp
+++ HotAirBalloon/src/HotAirBalloon/script.cpp
@@ -24,14 +24,16 @@ Vector3 playerPos;
 
 
  #pragma region Prompt Stuff
-Prompt ascendPrompt;
-Prompt descendPrompt;
-Prompt forwardPrompt;
-Prompt rotateLeftPrompt;
-Prompt rotateRightPrompt;
+Prompt prompt_ascend;
+Prompt prompt_descend;
+Prompt prompt_forward;
+Prompt prompt_rotateLeft;
+Prompt prompt_rotateRight;
 
 void register_prompt(Prompt &prompt, Hash control, const char* promptText)
 {
+	if (Config::bShowControls == false) return;
+
 	prompt = HUD::_UI_PROMPT_REGISTER_BEGIN();
 	HUD::_UI_PROMPT_SET_CONTROL_ACTION(prompt, control);
 	HUD::_UI_PROMPT_SET_TEXT(prompt, MISC::VAR_STRING(10, "LITERAL_STRING", promptText));
@@ -47,48 +49,60 @@ void register_prompt(Prompt &prompt, Hash control, const char* promptText)
 
 void toggle_prompts(bool enabled)
 {
+	if (Config::bShowControls == false) return;
+
 	// If ascendPrompt is valid, then all of them are valid
-	if (HUD::_UI_PROMPT_IS_VALID(ascendPrompt)) {
-		HUD::_UI_PROMPT_SET_VISIBLE(ascendPrompt, enabled);
-		HUD::_UI_PROMPT_SET_ENABLED(ascendPrompt, enabled);
-		HUD::_UI_PROMPT_SET_VISIBLE(descendPrompt, enabled);
-		HUD::_UI_PROMPT_SET_ENABLED(descendPrompt, enabled);
-		HUD::_UI_PROMPT_SET_VISIBLE(forwardPrompt, enabled);
-		HUD::_UI_PROMPT_SET_ENABLED(forwardPrompt, enabled);
-		HUD::_UI_PROMPT_SET_VISIBLE(rotateLeftPrompt, enabled);
-		HUD::_UI_PROMPT_SET_ENABLED(rotateLeftPrompt, enabled);
-		HUD::_UI_PROMPT_SET_VISIBLE(rotateRightPrompt, enabled);
-		HUD::_UI_PROMPT_SET_ENABLED(rotateRightPrompt, enabled);
+	if (HUD::_UI_PROMPT_IS_VALID(prompt_ascend)) {
+		HUD::_UI_PROMPT_SET_VISIBLE(prompt_ascend, enabled);
+		HUD::_UI_PROMPT_SET_ENABLED(prompt_ascend, enabled);
+		HUD::_UI_PROMPT_SET_VISIBLE(prompt_descend, enabled);
+		HUD::_UI_PROMPT_SET_ENABLED(prompt_descend, enabled);
+		HUD::_UI_PROMPT_SET_VISIBLE(prompt_forward, enabled);
+		HUD::_UI_PROMPT_SET_ENABLED(prompt_forward, enabled);
+		HUD::_UI_PROMPT_SET_VISIBLE(prompt_rotateLeft, enabled);
+		HUD::_UI_PROMPT_SET_ENABLED(prompt_rotateLeft, enabled);
+		HUD::_UI_PROMPT_SET_VISIBLE(prompt_rotateRight, enabled);
+		HUD::_UI_PROMPT_SET_ENABLED(prompt_rotateRight, enabled);
 	}
 }
  #pragma endregion
 
-
-Vehicle balloon;
-Blip blip;
 
 float RESISTANCE = 65.0f; // Higher = Slower you will go forward
 //float ASCEND_SPEED = 0.0f; // Higher = Faster ascend speed (DEFAULT MAX ASCEND SPEED = ~2.5f)
 float DESCEND_SPEED = 0.015f; // Higher = Faster descend speed (0.015f = ~-5.0f z velocity)
 float ROTATION_SPEED = 0.45f; // Higher = Faster rotation speed
 
-Hash KEY_ASCEND; // Shift
-Hash KEY_DESCEND; // Ctrl
-Hash KEY_FORWARD; // W
-Hash KEY_ROT_L; // A
-Hash KEY_ROT_R; // D
+const Hash KEY_ASCEND = INPUT_VEH_FLY_THROTTLE_UP; // Shift
+const Hash KEY_DESCEND = INPUT_VEH_HANDBRAKE; // Ctrl
+const Hash KEY_FORWARD = INPUT_VEH_MOVE_UP_ONLY; // W
+const Hash KEY_ROT_L = INPUT_VEH_MOVE_LEFT_ONLY; // A
+const Hash KEY_ROT_R = INPUT_VEH_MOVE_RIGHT_ONLY; // D
 
-// TODO: If rotating AND pressing forward, the balloon will want to go down if at certain angles. Find better Z..?
-// TODO: Hover mode
-// TODO: Should I enable veh controls while in balloon so I dont have to call PAD::IS_DISABLED_CONTROL_PRESSED?
-// TODO: Animations with player under burner
+const Hash BALLOON_MODEL = 0x5EB0BAE0; // HOTAIRBALLOON01
+Vehicle balloonVehicle;
+Blip balloonBlip;
+
+void delete_balloon(bool releaseModel)
+{
+	if (ENTITY::DOES_ENTITY_EXIST(balloonVehicle)) {
+		ENTITY::DELETE_ENTITY(&balloonVehicle);
+		if (releaseModel) {
+			STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(BALLOON_MODEL);
+		}
+	}
+}
 
 void create_balloon()
 {
-	if (!ENTITY::DOES_ENTITY_EXIST(balloon)) {
-		if (!STREAMING::HAS_MODEL_LOADED(MISC::GET_HASH_KEY("HOTAIRBALLOON01"))) {
-			while (!STREAMING::HAS_MODEL_LOADED(MISC::GET_HASH_KEY("HOTAIRBALLOON01"))) {
-				STREAMING::REQUEST_MODEL(MISC::GET_HASH_KEY("HOTAIRBALLOON01"), false);
+	if (ENTITY::DOES_ENTITY_EXIST(balloonVehicle)) {
+		delete_balloon(false);
+	}
+
+	if (!ENTITY::DOES_ENTITY_EXIST(balloonVehicle)) {
+		if (!STREAMING::HAS_MODEL_LOADED(BALLOON_MODEL)) {
+			while (!STREAMING::HAS_MODEL_LOADED(BALLOON_MODEL)) {
+				STREAMING::REQUEST_MODEL(BALLOON_MODEL, false);
 				WAIT(0);
 			}
 		}
@@ -99,49 +113,29 @@ void create_balloon()
 			playerPos.y += forward.y * 3.0f;
 		}
 
-		balloon = VEHICLE::CREATE_VEHICLE(MISC::GET_HASH_KEY("HOTAIRBALLOON01"), playerPos.x, playerPos.y, playerPos.z, ENTITY::GET_ENTITY_HEADING(playerPed), true, true, false, false);
-		VEHICLE::SET_VEHICLE_ON_GROUND_PROPERLY(balloon, true);
-		CAM::_DISABLE_CAM_COLLISION_FOR_ENTITY(balloon);
-		ENTITY::SET_ENTITY_DYNAMIC(balloon, true);
-		blip = MAP::BLIP_ADD_FOR_ENTITY(MISC::GET_HASH_KEY("BLIP_STYLE_COMPANION"), balloon);
+		balloonVehicle = VEHICLE::CREATE_VEHICLE(BALLOON_MODEL, playerPos.x, playerPos.y, playerPos.z, ENTITY::GET_ENTITY_HEADING(playerPed), true, true, false, false);
+		VEHICLE::SET_VEHICLE_ON_GROUND_PROPERLY(balloonVehicle, true);
+		ENTITY::SET_ENTITY_DYNAMIC(balloonVehicle, true);
+		balloonBlip = MAP::BLIP_ADD_FOR_ENTITY(MISC::GET_HASH_KEY("BLIP_STYLE_COMPANION"), balloonVehicle);
 
 		if (Config::bSpawnInBalloon) {
-			PED::SET_PED_INTO_VEHICLE(playerPed, balloon, -1);
+			PED::SET_PED_INTO_VEHICLE(playerPed, balloonVehicle, -1);
 		}
-		
-		//ENTITY::ATTACH_ENTITY_TO_ENTITY(playerPed, balloon, 0, 0.3f, -0.4f, 1.1f, 0.0f, 0.0f, 0.0f, false, false, false, false, 2, true, false, false);
 
-		// mfw when no enum eInputType
-		KEY_ASCEND = MISC::GET_HASH_KEY("INPUT_VEH_FLY_THROTTLE_UP"); // Shift
-		KEY_DESCEND = MISC::GET_HASH_KEY("INPUT_VEH_HANDBRAKE"); // Ctrl
-		KEY_FORWARD = MISC::GET_HASH_KEY("INPUT_VEH_MOVE_UP_ONLY"); // W
-		KEY_ROT_L = MISC::GET_HASH_KEY("INPUT_VEH_MOVE_LEFT_ONLY"); // A
-		KEY_ROT_R = MISC::GET_HASH_KEY("INPUT_VEH_MOVE_RIGHT_ONLY"); // D
-
-		if (Config::bShowControls) {
-			register_prompt(ascendPrompt, KEY_ASCEND, "Ascend");
-			register_prompt(descendPrompt, KEY_DESCEND, "Descend");
-			register_prompt(forwardPrompt, KEY_FORWARD, "Forward");
-			register_prompt(rotateLeftPrompt, KEY_ROT_L, "Rotate Left");
-			register_prompt(rotateRightPrompt, KEY_ROT_R, "Rotate Right");
-		}
+		register_prompt(prompt_ascend, KEY_ASCEND, "Ascend");
+		register_prompt(prompt_descend, KEY_DESCEND, "Descend");
+		register_prompt(prompt_forward, KEY_FORWARD, "Forward");
+		register_prompt(prompt_rotateLeft, KEY_ROT_L, "Rotate Left");
+		register_prompt(prompt_rotateRight, KEY_ROT_R, "Rotate Right");
 	}	
 }
 
 
 // So the balloon can't be moved or rotated while just sitting on the ground
+// Probably a better way to do this...
 bool shouldBalloonMove()
 {
-	return ENTITY::GET_ENTITY_HEIGHT_ABOVE_GROUND(balloon) > 0.125f;
-}
-
-
-void subtitle(const std::string& str)
-{
-	UILOG::_UILOG_SET_CACHED_OBJECTIVE(str.c_str());
-	UILOG::_UILOG_PRINT_CACHED_OBJECTIVE();
-	UILOG::_UILOG_CLEAR_HAS_DISPLAYED_CACHED_OBJECTIVE();
-	UILOG::_UILOG_CLEAR_CACHED_OBJECTIVE();
+	return ENTITY::GET_ENTITY_HEIGHT_ABOVE_GROUND(balloonVehicle) > 0.125f;
 }
 
 
@@ -160,12 +154,18 @@ void main()
 			create_balloon();
 		}
 
-		if (ENTITY::DOES_ENTITY_EXIST(balloon)) {
-			if (PED::IS_PED_IN_VEHICLE(playerPed, balloon, false)) {
-				Vector3 rot = ENTITY::GET_ENTITY_ROTATION(balloon, 2);
-				Vector3 velocity = ENTITY::GET_ENTITY_VELOCITY(balloon, -1);
+		if (ENTITY::DOES_ENTITY_EXIST(balloonVehicle))
+		{
+			if (PED::IS_PED_IN_VEHICLE(playerPed, balloonVehicle, false))
+			{
+				CAM::SET_GAMEPLAY_CAM_IGNORE_ENTITY_COLLISION_THIS_UPDATE(balloonVehicle);
+
+				Vector3 rot = ENTITY::GET_ENTITY_ROTATION(balloonVehicle, 2);
+				Vector3 velocity = ENTITY::GET_ENTITY_VELOCITY(balloonVehicle, -1);
 				Vector3 forward = ENTITY::GET_ENTITY_FORWARD_VECTOR(playerPed);
 
+				// Handle balloon rotation
+				// TODO: If rotating AND pressing forward, the balloon will want to go down if at certain angles. Find better Z..?
 				if (PAD::IS_DISABLED_CONTROL_PRESSED(0, KEY_ROT_L) && !PAD::IS_CONTROL_PRESSED(0, KEY_ROT_R)) {
 					// Rotate Left
 					rot.z += ROTATION_SPEED;
@@ -181,72 +181,59 @@ void main()
 					}
 				}
 
+				// Update rotation
 				if (shouldBalloonMove()) {
-					ENTITY::SET_ENTITY_ROTATION(balloon, rot.x, rot.y, rot.z, 2, false);
+					ENTITY::SET_ENTITY_ROTATION(balloonVehicle, rot.x, rot.y, rot.z, 2, false);
 				}
 
+				// Handle balloon XY velocity
 				if (PAD::IS_CONTROL_PRESSED(0, KEY_FORWARD)) {
 					if (shouldBalloonMove()) {
-						// This probably could be done better
 						// TODO: If rotating AND pressing forward, the balloon will want to go down if at certain angles. Find better Z..?
-						ENTITY::SET_ENTITY_VELOCITY(balloon, velocity.x + (forward.x / RESISTANCE), velocity.y + (forward.y / RESISTANCE), velocity.z);
+						ENTITY::SET_ENTITY_VELOCITY(balloonVehicle, velocity.x + (forward.x / RESISTANCE), velocity.y + (forward.y / RESISTANCE), velocity.z);
 					}
 				}
 
+				// Handle balloon Z velocity
 				if (PAD::IS_CONTROL_PRESSED(0, KEY_DESCEND) && !PAD::IS_CONTROL_PRESSED(0, KEY_ASCEND)) {
 					if (shouldBalloonMove()) {
-						ENTITY::SET_ENTITY_VELOCITY(balloon, velocity.x, velocity.y, velocity.z - DESCEND_SPEED);
+						ENTITY::SET_ENTITY_VELOCITY(balloonVehicle, velocity.x, velocity.y, velocity.z - DESCEND_SPEED);
 					}
 				}
-				//else if (!PAD::IS_CONTROL_PRESSED(0, KEY_DESCEND) && !PAD::IS_CONTROL_PRESSED(0, KEY_ASCEND) && !PAD::IS_CONTROL_PRESSED(0, KEY_FORWARD) && velocity.z <= 0.0f) {
-				//	if (shouldBalloonMove()) {
-				//		ENTITY::SET_ENTITY_VELOCITY(balloon, velocity.x, velocity.y, 0);
-				//	}
-				//}
-				//else if (PAD::IS_CONTROL_PRESSED(0, KEY_ASCEND) && !PAD::IS_CONTROL_PRESSED(0, KEY_DESCEND)) {
-				//	if (shouldBalloonMove()) {
-				//		ENTITY::SET_ENTITY_VELOCITY(balloon, velocity.x, velocity.y, velocity.z + ASCEND_SPEED);
-				//	}
-				//}
 
+				// Remove blip
+				if (!VEHICLE::IS_VEHICLE_DRIVEABLE(balloonVehicle, false, false)) {
+					MAP::REMOVE_BLIP(&balloonBlip);
+				}	
+			}
 
-				if (!VEHICLE::IS_VEHICLE_DRIVEABLE(balloon, false, false)) {
-					MAP::REMOVE_BLIP(&blip);
-				}
-
-				if (Config::bShowControls) {
-					toggle_prompts(true);
-				}
+			// Handle prompt visibility
+			if (ENTITY::DOES_ENTITY_EXIST(balloonVehicle) && PED::IS_PED_IN_MODEL(playerPed, BALLOON_MODEL) && PED::IS_PED_IN_VEHICLE(playerPed, balloonVehicle, false)) {
+				toggle_prompts(true);
 			}
 			else {
-				if (Config::bShowControls) {
-					toggle_prompts(false);
-				}
+				MAP::REMOVE_BLIP(&balloonBlip);
+				toggle_prompts(false);
 			}
 
-
-			Vector3 coords = ENTITY::GET_ENTITY_COORDS(balloon, false, false);
-			if (BUILTIN::VDIST2(playerPos.x, playerPos.y, playerPos.z, coords.x, coords.y, coords.z) > 50000.0f) {
+			// Delete the balloon
+			Vector3 balloonPos = ENTITY::GET_ENTITY_COORDS(balloonVehicle, false, false);
+			if (BUILTIN::VDIST2(playerPos.x, playerPos.y, playerPos.z, balloonPos.x, balloonPos.y, balloonPos.z) > 30000.0f) {
 				// No need to keep the balloon around if we're not near it
-				ENTITY::DELETE_ENTITY(&balloon);
+				delete_balloon(true);
 			}
 			
-
-			// VEHICLE::_0x7C9E45A4CED2E8DA() seems to make the balloon hover, but theres
-			// no way to make it stop havering it seems
-			
-			/*if (PAD::IS_CONTROL_JUST_PRESSED(0, MISC::GET_HASH_KEY("INPUT_HUD_SPECIAL"))) {
+			/*if (PAD::IS_CONTROL_JUST_PRESSED(0, INPUT_HUD_SPECIAL)) {
 				bHoverMode = !bHoverMode;
 				if (bHoverMode) {
-					subtitle("Hover mode activated. Press ~INPUT_HUD_SPECIAL~ to cancel.");
-					VEHICLE::_0x7C9E45A4CED2E8DA(balloon, 1.0f);
+					// Hover mode activated. Press ~INPUT_HUD_SPECIAL~ to cancel.
+					VEHICLE::_SET_BALLOON_HOVER_STATE(balloonVehicle, 1.0f);
 				} else {
-					subtitle("Hover mode deactivated. Press ~INPUT_HUD_SPECIAL~ to activate hover mode.");
-					VEHICLE::_0x7C9E45A4CED2E8DA(balloon, 0.0f); // This doesn't cancel hover?
+					// Hover mode deactivated. Press ~INPUT_HUD_SPECIAL~ to activate hover mode.
+					VEHICLE::_SET_BALLOON_HOVER_STATE(balloonVehicle, 0.0f); // This doesn't cancel hover?
 				}
 			}*/
 		}
-
 
 		playerPed = PLAYER::PLAYER_PED_ID();
 		playerPos = ENTITY::GET_ENTITY_COORDS(playerPed, true, false);
